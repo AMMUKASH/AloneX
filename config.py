852c1329a641695665c6ed17fa196115c3607226
𝐆𝛆𝛈𝛊𝛐𝛖𝛅ㅤ🏴‍☠️||鬼𝗚⽊𝛊神𝛐龙𝛅 🏴‍☠️ <misssqn@gmail.com>
2026-07-15 10:49:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,21 +6,21 @@ load_dotenv()
 
 class Config:
     def __init__(self):
-        self.API_ID = int(getenv("API_ID", "17596251"))
-        self.API_HASH = getenv("API_HASH", "e58343b4c0193e293e391daf97603fcd")
+        self.API_ID = int(getenv("API_ID", "38138069"))
+        self.API_HASH = getenv("API_HASH", "2ed313ebcc45cbcf65d1fc736ec71681")
 
-        self.BOT_TOKEN = getenv("BOT_TOKEN", "Apna Bot Token")
-        self.MONGO_URL = getenv("MONGO_URL", "Apna Mongo Db Dalo")
+        self.BOT_TOKEN = getenv("BOT_TOKEN", "8997247669:AAEhcPwhbtRwR3SxvVKsZU3bSnnS3RPO5xg")
+        self.MONGO_URL = getenv("MONGO_URL", " mongodb+srv://misssqn_db_user:Nova01@cluster0.6xxsrwq.mongodb.net/?retryWrites=true&w=majority")
 
-        self.LOGGER_ID = int(getenv("LOGGER_ID", "Apna Log Group Id Dalo"))
-        self.OWNER_ID = int(getenv("OWNER_ID", "Owner I'd dalo"))
+        self.LOGGER_ID = int(getenv("LOGGER_ID", "-1003947649552"))
+        self.OWNER_ID = int(getenv("OWNER_ID", "8724182918"))
         
-        self.SESSION1 = getenv("SESSION", "Apna String Dalo")
+        self.SESSION1 = getenv("SESSION", "AQJF8NUAGQeFi5yiBZDNr6Hj8ldBLHyZNxe6aCY4iznu5GCaSvhIJKYnYv219DQY52VtZ0q8pjTOk1VsrqkA8ZZXkJZGlxazx9Ekn8dS9AgHQQhKkhxxvGzwr6LlZhSB4LX31wKM-uXfQjL3Hh7NwlocBhxHq2dTWeC8ZCpw60Q_f0ppnRyWoY2jClVE3C-xa6yhxMh84mLP9YwAm99UWCNi-RPAFF1q1J8PjvV74e0y69k8OO2ucyfVgMu3SHWKinSS7a5m2zvbCrjglZHZsMVQJwy0Q6Ywpn1HdgA7WsAdJBTXcdJoccgN80bPjlbPVwpvQqhZnBD0uiSbPCbis8pesLJcDAAAAAH_hA2LAA")
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
-        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AloneUpdates")
-        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AloneBotSupport")
+        self.SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NovaBot_Support")
+        self.SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/NovaBot_Support")
 
         self.AUTO_END: bool = getenv("AUTO_END", False)
         self.AUTO_LEAVE: bool = getenv("AUTO_LEAVE", False)
